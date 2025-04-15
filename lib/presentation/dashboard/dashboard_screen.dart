2aa7f9daa1afe4629e514b8c3c1f1b7518706e2c
IbrarRaj <ibrarhassan08209@gmail.com>
2025-04-15 22:54:46 +0530

    dashboard ui

--- lib/presentation/dashboard/dashboard_screen.dart
+++ lib/presentation/dashboard/dashboard_screen.dart
@@ -6,6 +6,7 @@ import 'package:dashhit/common/widgets/page_view_chart.dart';
 import 'package:dashhit/common/widgets/session_gauge.dart';
 import 'package:dashhit/provider/data_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -58,48 +59,57 @@ class DashboardScreen extends StatelessWidget {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          spacing: 20,
-          children: [
-            // Metrics Row
-            Row(
-              children: [
-                MetricCard(
-                  title: 'Active Users',
-                  value:
-                      Provider.of<DataProvider>(context).activeUsers.toString(),
-                  image: ImageConstants.user,
-                  color: Colors.blue,
-                ),
-                MetricCard(
-                  title: 'Page Views',
-                  value:
-                      Provider.of<DataProvider>(context).pageViews.toString(),
-                  image: ImageConstants.eyeVisibility,
-                  color: Colors.green,
-                ),
-              ],
-            ),
-            // Charts Row
-            Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: PageViewChart(),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: SessionGauge(),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Using ${Provider.of<DataProvider>(context).useWebSocket ? 'WebSocket' : 'HTTP Polling'}',
-              style: const TextStyle(color: Colors.grey),
-            ),
-          ],
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Metrics Row
+              SizedBox(
+                height: 250,
+                child: Lottie.asset(ImageConstants.analyticsLot),
+              ),
+              Row(
+                children: [
+                  MetricCard(
+                    title: 'Active Users',
+                    value: Provider.of<DataProvider>(context)
+                        .activeUsers
+                        .toString(),
+                    image: ImageConstants.user,
+                    color: Colors.blue,
+                  ),
+                  MetricCard(
+                    title: 'Page Views',
+                    value:
+                        Provider.of<DataProvider>(context).pageViews.toString(),
+                    image: ImageConstants.eyeVisibility,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              // Charts Row
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: PageViewChart(),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: SessionGauge(),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Using ${Provider.of<DataProvider>(context).useWebSocket ? 'WebSocket' : 'HTTP Polling'}',
+                style: const TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
         ),
       ),
     );
